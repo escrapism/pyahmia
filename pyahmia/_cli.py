e412f42a5f022c6d6828aa8df38f1177a6606923
Ritchie Mwewa <74001397+rly0nheart@users.noreply.github.com>
2025-11-28 14:06:55 +0200

    Fix issue #9

--- pyahmia/_cli.py
+++ pyahmia/_cli.py
@@ -62,16 +62,26 @@ def cli(
             "[bold]Initialising[/bold][yellow]...[/yellow]", console=console
         ) as status:
             client.check_updates(status=status)
+
+            # ----------------------------------------------------------------------- #
+            status.update(
+                f"[bold]Capturing session token. Please wait[yellow]...[/bold][/yellow]"
+            )
+            token = client.token()
+            # ----------------------------------------------------------------------- #
+
+            # ----------------------------------------------------------------------- #
             status.update(
                 f"[bold]Searching for [#c7ff70]{query}[/]. Please wait[yellow]...[/bold][/yellow]"
             )
-
-            search = client.search(query=query, time_period=period)
+            search = client.search(query=query, time_period=period, token=token)
+            # ----------------------------------------------------------------------- #
 
             if search.success:
                 results = search.results
                 console.log(f"[bold][#c7ff70]✔[/] {search.message}[/bold]")
                 for index, result in enumerate(results, start=1):
+                    # ----------------------------------------------------------------------- #
                     content_items = [
                         f"[bold][#c7ff70]{result.title}[/][/bold]",
                         Rule(style="#444444"),
@@ -87,6 +97,7 @@ def cli(
                             title=f"[{index}]",
                         )
                     )
+                    # ----------------------------------------------------------------------- #
 
                 if export:
                     outfile: str = client.export_csv(results=results, path=query)
